ac38719e8f59ef2ca5efb05c669db69ff4c93984
Aylur <k.demeter@protonmail.com>
2025-12-04 01:34:18 +0100

    chore: migrate to pnpm

--- auto-always-on-top/src/prefs/index.tsx
+++ auto-always-on-top/src/prefs/index.tsx
@@ -1,19 +1,19 @@
 import Adw from "gi://Adw"
 import PreferencesPage from "./PreferencesPage"
 import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js"
-import { This } from "gnim"
+import { This, createRoot } from "gnim"
 import { createExtensionSettings, SettingsContext } from "~schemas"
 
 export default class extends ExtensionPreferences {
   async fillPreferencesWindow(window: Adw.PreferencesWindow): Promise<void> {
     const settings = createExtensionSettings(this.getSettings())
 
-    void (
-      <This this={window}>
+    createRoot((dispose) => (
+      <This this={window} onCloseRequest={dispose}>
         <SettingsContext value={settings}>
           {() => <PreferencesPage window={window} />}
         </SettingsContext>
       </This>
-    )
+    ))
   }
 }
